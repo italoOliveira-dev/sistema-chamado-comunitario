d06054b4282917903fcebbc2aa1718a7b3707aeb
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-09-08 20:54:47 -0300

    feat: inicio do serviço de usuários

---
+++ servico-usuario/src/main/java/com/projeto/integracao/servico_usuario/ServicoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.integracao.servico_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoUsuarioApplication.class, args);
+	}
+
+}
